b25817aa830748cb6b8bfc493f9afdb9dc3e0045
Zian Carlos Wong <ziancrlswong@gmail.com>
2026-08-19 14:30:28 +0700

    docker container

--- 7. Jobs And Docker Container/starting-project/data/database.js
+++ 7. Jobs And Docker Container/starting-project/data/database.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
@@ -9,16 +9,21 @@ const dbName = process.env.MONGODB_DB_NAME;
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
-console.log('Trying to connect to db');
+console.log("Trying to connect to db");
 
+print(connectionProtocol);
+print(clusterAddress);
+print(dbUser);
+print(dbPassword);
+print(dbName);
 try {
   await client.connect();
   await client.db(dbName).command({ ping: 1 });
-  console.log('Connected successfully to server');
+  console.log("Connected successfully to server");
 } catch (error) {
-  console.log('Connection failed.');
+  console.log("Connection failed.");
   await client.close();
-  console.log('Connection closed.');
+  console.log("Connection closed.");
   process.exit(1);
 }
 
